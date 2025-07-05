cedfb806e9e929fb056a236cb68e0c290d69a4c8
kommaks <56777586+kommaks@users.noreply.github.com>
2025-07-05 16:39:47 +0300

    Update run_array.sh

--- run_array.sh
+++ run_array.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -p gpu                   # partition
-#SBATCH --array=0-11%4           # 12 задач, max 4 в параллель
-#SBATCH --nodes=1                # одна нода на под-задачу
-#SBATCH --ntasks=1               # одна task  (= один процесс python)
-#SBATCH --cpus-per-task=4        # 4 CPU-потока под DataLoader, т.п.
-#SBATCH --gpus-per-task=1        # 1 GPU
-#SBATCH --mem=80G                # RAM на узел
-#SBATCH -t 1-00:00:00            # 1 day wall-time
-#SBATCH -o logs/%A_%a.out        # stdout+stderr > logs/JobID_ArrayID.out
+#SBATCH -p gpu                  
+#SBATCH --array=0-11%4          
+#SBATCH --nodes=1                
+#SBATCH --ntasks=1               
+#SBATCH --cpus-per-task=4        
+#SBATCH --gpus-per-task=1        
+#SBATCH --mem=80G                
+#SBATCH -t 1-00:00:00            
+#SBATCH -o logs/%A_%a.out        
 #SBATCH -e logs/%A_%a.err
 
-# --- список датасетов в том же порядке, что в YAML ---
+
 datasets=( delucionqa finqa cuad covidqa emanual expertqa 
            hagrid hotpotqa msmarco pubmedqa tatqa techqa )
 
